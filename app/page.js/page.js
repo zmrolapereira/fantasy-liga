f54105a7eb9c15c03014af847b73cebca37ed3d4
zmrolapereira <zmrolapereira@gmail.com>
2025-10-17 01:16:05 +0000

    navbar + sobre + contactos

---
+++ app/page.js/page.js
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main style={{display:'grid',placeItems:'center',minHeight:'100vh'}}>
+      <h1>Bem-vindo à Fantasy Liga Portugal!</h1>
+    </main>
+  );
+}
